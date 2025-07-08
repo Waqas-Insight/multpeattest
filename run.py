b3bd2329559f09eed81fdc0890b757e37f8d5495
Waqas-Insight <161314190+Waqas-Insight@users.noreply.github.com>
2025-07-08 12:06:30 +0100

    Update run.py
    updated upload file path

--- run.py
+++ run.py
@@ -700,7 +700,7 @@ def save_csv():
         print("Exception occurred:", e)
         return jsonify({"error": str(e)}), 500
     
-UPLOAD_FOLDER = "/home/ales/multipeattools/csv_outputs"
+UPLOAD_FOLDER = "/home/ales/multipeattools/"
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 def get_latest_csv(username):
@@ -716,7 +716,7 @@ def get_latest_csv(username):
     latest_file = max(valid_files, key=os.path.getctime)  # Get the most recent file
     return latest_file  # Return only the latest file path
 
-UPLOAD_FOLDER = "/home/ales/multipeattools/csv_outputs"
+UPLOAD_FOLDER = "/home/ales/multipeattools/"
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 def get_latest_csv(username):
