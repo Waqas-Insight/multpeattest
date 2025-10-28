201d18dba52dbc1891e28f69b2033a09927434eb
waqas <root@DSIs-MBP.ie.deri.local>
2025-10-28 10:59:49 +0000

    update_host

--- static/scripts/calc_ffp.js
+++ static/scripts/calc_ffp.js
@@ -616,7 +616,7 @@ function saveCSVData(rows) {
         })
     };
 
-    fetch('http://127.0.0.1:5000/save-csv', requestOptions)
+    fetch('https://test-multipeat.insight-centre.org/save-csv', requestOptions)
         .then(response => {
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
@@ -660,7 +660,7 @@ function saveCSVData(rows) {
 function retrieveCSVData() {
     console.log("Retrieving CSV Data...");
 
-    fetch('http://127.0.0.1/get-csv', {
+    fetch('https://test-multipeat.insight-centre.org/get-csv', {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json; charset=UTF-8'
